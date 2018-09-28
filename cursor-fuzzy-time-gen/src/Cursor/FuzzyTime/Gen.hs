f76435e24e0d98fb7c73b513c7389ec58fd90fb8
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2018-09-28 05:15:43 +0200

    Initial commit

---
+++ cursor-fuzzy-time-gen/src/Cursor/FuzzyTime/Gen.hs
@@ -0,0 +1,8 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Cursor.FuzzyTime.Gen where
+
+import Data.GenValidity
+import Data.GenValidity.Time ()
+
+import Cursor.FuzzyTime
---
+++ cursor-fuzzy-time/src/Cursor/FuzzyTime.hs
@@ -0,0 +1,3 @@
+module Cursor.FuzzyTime
+    (
+    ) where
