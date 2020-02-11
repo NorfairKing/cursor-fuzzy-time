c4c41cd9e5f0461467e91dfd84da25986aa568ab
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-02-11 01:03:43 +0100

    ready to release

--- nix/fuzzy-time-version.nix
+++ nix/fuzzy-time-version.nix
@@ -1,6 +1,6 @@
 {
   owner = "NorfairKing";
   repo = "fuzzy-time";
-  rev = "040879ea5dc3ce15d946a92d6d671e27c1c205fd";
-  sha256 = "sha256:0xhrfci03a1qc1ay9qb0rsmlhipn2pv6sz5bwfxw4lppkb1kf6gh";
+  rev = "e838407dbfa6fb65786a8eca419abafa13bfdc54";
+  sha256 = "sha256:1053mgqnkbsr9hcppck1m43qv2fjg70sqqdmqbgm36l8682b023z";
 }
