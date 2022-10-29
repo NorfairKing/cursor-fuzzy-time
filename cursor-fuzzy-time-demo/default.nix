af5583fb4ccb9de6107ba7e907f733a37befbb6a
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-10-29 15:15:33 +0200

    Nix flake

---
+++ cursor-fuzzy-time-demo/default.nix
@@ -0,0 +1,17 @@
+{ mkDerivation, base, brick, cursor, cursor-fuzzy-time, directory
+, fuzzy-time, lib, microlens, text, time, vty
+}:
+mkDerivation {
+  pname = "cursor-fuzzy-time-demo";
+  version = "0.0.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    base brick cursor cursor-fuzzy-time directory fuzzy-time microlens
+    text time vty
+  ];
+  executableHaskellDepends = [ base ];
+  homepage = "https://github.com/NorfairKing/picosmos#readme";
+  license = lib.licenses.mit;
+}
--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,20 +1,26 @@
-final:
-previous:
+final: prev:
+with final.lib;
 with final.haskell.lib;
 {
-  cursorFuzzyTimePackages =
-    let cursorFuzzyTimePkg = name:
-      (buildStrictly (final.haskellPackages.callCabal2nixWithOptions name (../. + "/${name}") "--no-hpack" { }));
-    in
-    final.lib.genAttrs [
-      "cursor-fuzzy-time"
-      "cursor-fuzzy-time-gen"
-      "cursor-fuzzy-time-demo"
-    ]
-      cursorFuzzyTimePkg;
-  haskellPackages = previous.haskellPackages.override (old: {
-    overrides = final.lib.composeExtensions (old.overrides or (_: _: { })) (
-      self: super: final.cursorFuzzyTimePackages
+  cursorFuzzyTimeRelease =
+    final.symlinkJoin {
+      name = "cursor-fuzzy-time-release";
+      paths = attrValues final.haskellPackages.cursorFuzzyTimePackages;
+    };
+
+  haskellPackages = prev.haskellPackages.override (old: {
+    overrides = composeExtensions (old.overrides or (_: _: { })) (
+      self: super:
+        let cursorFuzzyTimePackages =
+          let cursorFuzzyTimePkg = name: buildStrictly (self.callPackage (../${name}) { });
+          in
+          final.lib.genAttrs [
+            "cursor-fuzzy-time"
+            "cursor-fuzzy-time-gen"
+            "cursor-fuzzy-time-demo"
+          ]
+            cursorFuzzyTimePkg;
+        in { inherit cursorFuzzyTimePackages; } // cursorFuzzyTimePackages
     );
   });
 }
