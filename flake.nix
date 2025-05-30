c5e84b22887bbc513addd0c95d979a5c16211d52
Tom Sydney Kerckhove <syd@cs-syd.eu>
2025-05-30 10:32:30 +0200

    Upgrade to nixos-25.05 and lts 23.19

--- flake.nix
+++ flake.nix
@@ -1,25 +1,20 @@
 {
   description = "cursor-fuzzy-time";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-23.11";
-    nixpkgs-23_05.url = "github:NixOS/nixpkgs?ref=nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.05";
+    nixpkgs-24_11.url = "github:NixOS/nixpkgs?ref=nixos-24.11";
+    nixpkgs-24_05.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
-    validity.url = "github:NorfairKing/validity";
-    validity.flake = false;
-    cursor.url = "github:NorfairKing/cursor";
-    cursor.flake = false;
     fuzzy-time.url = "github:NorfairKing/fuzzy-time";
-    fuzzy-time.flake = false;
   };
 
   outputs =
     { self
     , nixpkgs
-    , nixpkgs-23_05
+    , nixpkgs-24_11
+    , nixpkgs-24_05
     , pre-commit-hooks
-    , validity
     , fuzzy-time
-    , cursor
     }:
     let
       system = "x86_64-linux";
@@ -27,9 +22,8 @@
         inherit system;
         overlays = [
           self.overlays.${system}
-          (import (validity + "/nix/overlay.nix"))
-          (import (cursor + "/nix/overlay.nix"))
           (import (fuzzy-time + "/nix/overlay.nix"))
+
         ];
       };
       pkgs = pkgsFor nixpkgs;
@@ -44,7 +38,8 @@
             in pkgs'.cursorFuzzyTimeRelease;
           allNixpkgs = {
             inherit
-              nixpkgs-23_05;
+              nixpkgs-24_11
+              nixpkgs-24_05;
           };
           backwardCompatibilityChecks = pkgs.lib.mapAttrs (_: nixpkgs: backwardCompatibilityCheckFor nixpkgs) allNixpkgs;
         in
@@ -67,17 +62,9 @@
         withHoogle = true;
         doBenchmark = true;
         buildInputs = with pkgs; [
-          niv
           zlib
           cabal-install
-        ] ++ (with pre-commit-hooks.packages.${system};
-          [
-            hlint
-            hpack
-            nixpkgs-fmt
-            ormolu
-            cabal2nix
-          ]);
+        ] ++ self.checks.${system}.pre-commit.enabledPackages;
         shellHook = self.checks.${system}.pre-commit.shellHook;
       };
     };
